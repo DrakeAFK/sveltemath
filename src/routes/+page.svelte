dbd175966b0292967f9fd8e8675b5f2a077e932b
DrakeAFK <drakehopkins@outlook.com>
2024-02-12 14:29:21 -0500

    add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+  let answer = 0;
+  let num1 = 0;
+  let num2 = 0;
+</script>
+
 <div class="text-center my-5">
   <h1 class="text-2xl font-mono text-white">Svelte Math</h1>
   <p class="">Math doesn't have to be hard</p>
+</div>
+
+<div>
+  
 </div>
